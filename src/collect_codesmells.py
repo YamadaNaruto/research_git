39b23b576de84a1f623a73a81dc465ced58a0c95
Naruto Yamada <is0748vi@ed.ritsumei.ac.jp>
2026-02-18 11:06:37 +0900

    add commit time and delete folder

--- src/collect_codesmells.py
+++ src/collect_codesmells.py
@@ -1,11 +1,13 @@
 #git log からコミットごとにコードスメルを検出してCSVに保存するスクリプト
 import subprocess
 import os
+import shutil
 from pathlib import Path
-from collect_testsmels import get_hashes_of_file,get_content_file_at_commit
+from collect_testsmels import get_hashes_of_file,get_content_file_at_commit,get_commit_time
 
 
 hashes = get_hashes_of_file("example_codesmells.py")#解析するファイル指定
+times = get_commit_time("example_codesmells.py")#コミット時刻を取得
 base_tmp=Path("/Users/yamadanaruto/Desktop/mothertests")
 base_tmp.mkdir(exist_ok=True)
 i=0
@@ -15,10 +17,10 @@ for commits_hash in hashes:
     #print(f'Commit: {commits_hash}\nContent:\n{content}\n')    
 
 #ファイルに内容を書き込む
-    path = f'/Users/yamadanaruto/research_git/src/mothertests/codeing/code_example{i}.py'#書き込む場所
-    f = open(path, 'w')
-    f.write(content)  
-    f.close()
+    path = Path(f'/Users/yamadanaruto/research_git/src/mothertests/codeing/{times[i]}.py')
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(content)
+    
     i+=1
         #コードスメル検出するコマンドを実行
         #sonarpropertiesのinclutionのとこを解析したいファイル(フォルダ)に指定
@@ -28,7 +30,10 @@ result = subprocess.run(
                     text=True,
                     check=True
              )
-                         
+folder_path = Path(f"/Users/yamadanaruto/research_git/src/mothertests/codeing")
+if folder_path.exists():
+    shutil.rmtree(folder_path)
+    print("フォルダごと削除しました")                        
 
         
             
