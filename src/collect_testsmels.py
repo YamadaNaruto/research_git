50f76cf0590ae327284594dca418741a113d168b
Naruto Yamada <is0748vi@ed.ritsumei.ac.jp>
2026-02-18 09:27:55 +0900

    record commit time

--- src/collect_testsmels.py
+++ src/collect_testsmels.py
@@ -15,8 +15,15 @@ def get_hashes_of_file(file_path):
     )
     return result.stdout.splitlines()
 #print(get_hashes_of_file('test_utils.py'))
-
-
+def get_commit_time(file_path):
+    """コミットの時刻を入手する"""
+    result = subprocess.run(
+        ['git','log','--pretty=format:%ad','--',file_path],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    return result.stdout.splitlines()
 
 #ファイルの中身を一時的にファイルに保存してテストスメル検出スクリプトに渡す
 def get_content_file_at_commit(commit_hash, file_path):
@@ -30,8 +37,9 @@ def get_content_file_at_commit(commit_hash, file_path):
     return content
        
 def main():
-    hashes = get_hashes_of_file('mothertests/testing/test_example0.py')#現在の階層も含んだパス
-   
+    hashes = get_hashes_of_file('mothertests/testing/test_example0.py')#対象ファイルのパス
+    times = get_commit_time('mothertests/testing/test_example0.py')
+    print(times)
     i=0
     for commits_hash in hashes:
         
@@ -39,9 +47,10 @@ def main():
         #print(f'Commit: {commits_hash}\nContent:\n{content}\n')    
 
     #ファイルに内容を書き込む
-        path = Path(f'/Users/yamadanaruto/Desktop/mothertests/testing{i}/test_example{i}.py')
+        path = Path(f'/Users/yamadanaruto/Desktop/mothertests/{times[i]}/test_{i}.py')
         path.parent.mkdir(parents=True, exist_ok=True)
         path.write_text(content)
+        
          
        
             #テストスメル検出するコマンドを実行
@@ -51,12 +60,12 @@ def main():
                             text=True,
                             check=True
                     )
-        folder_path = Path(f"/Users/yamadanaruto/Desktop/mothertests/testing{i}")
+        folder_path = Path(f"/Users/yamadanaruto/Desktop/mothertests/{times[i]}")
         if folder_path.exists():
             shutil.rmtree(folder_path)
             print("フォルダごと削除しました")
             i+=1
-
+  
 if __name__ == "__main__":
     main()
 
