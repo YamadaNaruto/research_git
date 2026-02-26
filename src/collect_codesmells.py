0a68e199e4539f63abc311c1741338e880cb608d
Naruto Yamada <is0748vi@ed.ritsumei.ac.jp>
2026-02-26 09:28:56 +0900

    add  file_path

--- src/collect_codesmells.py
+++ src/collect_codesmells.py
@@ -5,19 +5,19 @@ import shutil
 from pathlib import Path
 from collect_testsmels import get_hashes_of_file,get_content_file_at_commit,get_commit_time
 
+file_path ="analyzer/codechecker_analyzer/buildlog/build_action.py"
+hashes = get_hashes_of_file(file_path)#解析するファイル指定（現在の階層含まない
+times = get_commit_time(file_path)#コミット時刻を取得(現在の階層含まない)
+print(times)
 
-hashes = get_hashes_of_file("example_codesmells.py")#解析するファイル指定
-times = get_commit_time("example_codesmells.py")#コミット時刻を取得
-base_tmp=Path("/Users/yamadanaruto/Desktop/mothertests")
-base_tmp.mkdir(exist_ok=True)
 i=0
 for commits_hash in hashes:
     
-    content = get_content_file_at_commit (commits_hash, 'src/example_codesmells.py')
+    content = get_content_file_at_commit (commits_hash, file_path)#現在の階層から
     #print(f'Commit: {commits_hash}\nContent:\n{content}\n')    
 
 #ファイルに内容を書き込む
-    path = Path(f'/Users/yamadanaruto/research_git/src/mothertests/codeing/{times[i]}.py')
+    path = Path(f'/Users/yamadanaruto/research_git/codechecker/mothertests/codeing/{times[i]}.py')
     path.parent.mkdir(parents=True, exist_ok=True)
     path.write_text(content)
     
@@ -30,7 +30,7 @@ result = subprocess.run(
                     text=True,
                     check=True
              )
-folder_path = Path(f"/Users/yamadanaruto/research_git/src/mothertests/codeing")
+folder_path = Path(f"/Users/yamadanaruto/research_git/codechecker/mothertests/codeing")
 if folder_path.exists():
     shutil.rmtree(folder_path)
     print("フォルダごと削除しました")                        
